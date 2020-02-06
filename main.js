4da09394d095f171c37bc2cc93647401a65ddf8b
20200509 <taijayyy@gmail.com>
2020-02-06 23:02:51 +0900

    ani

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-window.addEventListener('load', function () {
+(function () {
+	var loading = document.querySelector('#loading')
 	var invitation = document.querySelector('#invitation')
 	var next = document.querySelector('.next')
 	var prev = document.querySelector('.prev')
-	invitation.style.height = window.innerHeight + 'px'
-
 	var flicking = new eg.Flicking('.wrapper', {
-    circular: true,
-    gap: 10,
+		circular: true,
+		gap: 10,
 		renderOnlyVisible: true,
 		autoResize: true
 	})
-
+	
+	invitation.style.height = window.innerHeight + 'px'
+	
 	next.addEventListener('click', function (e) {
 		e.preventDefault()
 		flicking.next()
 	})
-
+	
 	prev.addEventListener('click', function (e) {
 		e.preventDefault()
 		flicking.prev()
 	})
-})
+	
+	window.addEventListener('load', function () {
+		setTimeout(function () {
+			invitation.style.opacity = '1'
+			loading.classList.add('hide')
+		}, 2000)
+	})
+})()
