846693c0467e75a4d65e3c1ed572505c7eb2277d
20200509 <taijayyy@gmail.com>
2020-03-22 00:14:38 +0900

    media query

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 (function () {
-	window.addEventListener('load', function () {
-		// D-Day 설정
-		var el = document.getElementById('d-day')
-		var wedding = new Date('2020-05-09')
-		var today = new Date()
-		var gap = today.getTime() - wedding.getTime()
-		var result = Math.ceil(gap / (1000 * 60 * 60 * 24)) * -1
-		el.innerText = result
+	// D-Day 설정
+	var el = document.getElementById('d-day')
+	var wedding = new Date('2020-05-09')
+	var today = new Date()
+	var gap = today.getTime() - wedding.getTime()
+	var result = Math.ceil(gap / (1000 * 60 * 60 * 24)) * -1
+	el.innerText = result
 
-		// 지도 설정
-		// var container = document.getElementById('map')
-		// var options = {
-		// 	center: new kakao.maps.LatLng(37.560310, 126.967112),
-		// 	level: 3
-		// }
+	// 지도 설정
+	var container = document.getElementById('map')
+	var options = {
+		center: new kakao.maps.LatLng(37.560310, 126.967112),
+		level: 3
+	}
 
-		// var map = new kakao.maps.Map(container, options)
-	})
+	var map = new kakao.maps.Map(container, options)
 })()
