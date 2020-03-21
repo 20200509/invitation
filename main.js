e951bc82edfc839b2544d234687d882e861037c1
April.Kim <april.kim@obigo.com>
2020-03-21 22:11:05 +0900

    invitation

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
 (function () {
-	var loading = document.querySelector('#loading')
-	var invitation = document.querySelector('#invitation')
-	var next = document.querySelector('.next')
-	var prev = document.querySelector('.prev')
-	var flicking = new eg.Flicking('.wrapper', {
-		circular: true,
-		gap: 10,
-		renderOnlyVisible: true,
-		autoResize: true
-	})
-	
-	invitation.style.height = window.innerHeight + 'px'
-	
-	next.addEventListener('click', function (e) {
-		e.preventDefault()
-		flicking.next()
-	})
-	
-	prev.addEventListener('click', function (e) {
-		e.preventDefault()
-		flicking.prev()
-	})
-	
 	window.addEventListener('load', function () {
-		setTimeout(function () {
-			invitation.style.opacity = '1'
-			loading.classList.add('hide')
-		}, 2000)
+		var el = document.getElementById('d-day')
+		var wedding = new Date('2020-05-09')
+		var today = new Date()
+		var gap = today.getTime() - wedding.getTime()
+		var result = Math.ceil(gap / (1000 * 60 * 60 * 24)) * -1
+		el.innerText = result
+
+		var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+		var options = { //지도를 생성할 때 필요한 기본 옵션
+			center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+			level: 3 //지도의 레벨(확대, 축소 정도)
+		};
+
+		var map = new kakao.maps.Map(container, options);
 	})
 })()
