a598cdbda0ece927935beddcc45d10c6d67e0edc
20200509 <taijayyy@gmail.com>
2020-03-21 23:59:07 +0900

    media query

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 (function () {
 	window.addEventListener('load', function () {
+		// D-Day 설정
 		var el = document.getElementById('d-day')
 		var wedding = new Date('2020-05-09')
 		var today = new Date()
@@ -7,12 +8,13 @@
 		var result = Math.ceil(gap / (1000 * 60 * 60 * 24)) * -1
 		el.innerText = result
 
-		var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-		var options = { //지도를 생성할 때 필요한 기본 옵션
-			center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-			level: 3 //지도의 레벨(확대, 축소 정도)
-		};
+		// 지도 설정
+		// var container = document.getElementById('map')
+		// var options = {
+		// 	center: new kakao.maps.LatLng(37.560310, 126.967112),
+		// 	level: 3
+		// }
 
-		var map = new kakao.maps.Map(container, options);
+		// var map = new kakao.maps.Map(container, options)
 	})
 })()
