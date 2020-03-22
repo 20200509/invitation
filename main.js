938583463ee93ec048e1d57ab116b5efd92daaaa
20200509 <taijayyy@gmail.com>
2020-03-22 12:44:11 +0900

    photo details

--- main.js
+++ main.js
@@ -8,11 +8,39 @@
 	el.innerText = result
 
 	// 지도 설정
-	var container = document.getElementById('map')
-	var options = {
-		center: new kakao.maps.LatLng(37.560310, 126.967112),
-		level: 3
+	// var container = document.getElementById('map')
+	// var options = {
+	// 	center: new kakao.maps.LatLng(37.560310, 126.967112),
+	// 	level: 3
+	// }
+
+	// var map = new kakao.maps.Map(container, options)
+
+	// 사진 클릭
+	var photoFrame = document.getElementById('photo-frame')
+	var photoDetail = document.getElementById('photo-detail')
+	photoFrame.addEventListener('click', function (e) {
+		if (e.target.classList.contains('thumbnail')) {
+			openPhotoDetail()
+		}
+	})
+
+	photoDetail.addEventListener('click', function (e) {
+		closePhotoDetail()
+	})
+
+	function openPhotoDetail () {
+		var body = document.getElementsByTagName('body')[0]
+		
+		photoDetail.setAttribute('style', 'top: ' + window.pageYOffset + 'px;')
+		body.classList.add('scroll-disabled')
+		photoDetail.classList.remove('hide')
 	}
 
-	var map = new kakao.maps.Map(container, options)
+	function closePhotoDetail () {
+		var body = document.getElementsByTagName('body')[0]
+
+		body.classList.remove('scroll-disabled')
+		photoDetail.classList.add('hide')
+	}
 })()
